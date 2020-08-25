b9f700239a2be99eec0b2733fa894dd01fe8d196
Mattlau04 <37871312+Mattlau04@users.noreply.github.com>
2020-08-25 10:53:23 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,2 +1,8 @@
-while True:
-    print("Chimp event" + ''.join((chr(7) for x in range(666))))
+import sys
+try:
+  if sys.argv[1] == "cock":
+    while True:
+        print("Lmao you clicked the button you osu player" + ''.join((chr(7) for x in range(666))))
+else:
+    while True:
+        print("Chimp event" + ''.join((chr(7) for x in range(666))))
