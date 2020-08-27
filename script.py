0c47489b7764935c76554a82c3237f3e3ccf539a
Blakeando <52489043+Blakeando@users.noreply.github.com>
2020-08-27 18:23:09 +1000

    fixed bug in the mainframe

--- script.py
+++ script.py
@@ -1,8 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Author: Blakeando
+# @Date:   2020-08-27 18:13:16
+# @Last Modified by:   Blakeando
+# @Last Modified time: 2020-08-27 18:21:35
 import sys
 try:
-  if sys.argv[1] == "cock":
-    while True:
-        print("Lmao you clicked the button you osu player" + ''.join((chr(7) for x in range(666))))
-else:
-    while True:
-        print("Chimp event" + ''.join((chr(7) for x in range(666))))
+    if sys.argv[1] == "cock":
+        while True:
+            print(' '.join("You play osu" for x in range(60)) + ''.join((chr(7) for x in range(666))))
+    else:
+        while True:
+            print("Chimp event" + ''.join((chr(7) for x in range(666))))
