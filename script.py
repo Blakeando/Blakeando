e1648f601dfa3dfa6908dc5377b8eca8d4d82027
Blakeando <52489043+Blakeando@users.noreply.github.com>
2020-08-27 18:28:38 +1000

    we asciiing

--- script.py
+++ script.py
@@ -2,13 +2,13 @@
 # @Author: Blakeando
 # @Date:   2020-08-27 18:13:16
 # @Last Modified by:   Blakeando
-# @Last Modified time: 2020-08-27 18:25:43
+# @Last Modified time: 2020-08-27 18:28:28
 import random
 
 while True:
     print(
         " ".join(
-            random.choice(["You play osu", "Chimpe event", "monke"]) for x in range(60)
+            random.choice(["You play osu", "Chimpe event", "monke", (chr(random.randint(0, 99999)) for y in range(random.randint(46, 405))]) for x in range(60)
         )
         + "".join((chr(7) for x in range(666)))
     )
